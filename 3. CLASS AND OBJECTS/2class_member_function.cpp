cf1ac8974abb49099c8a90c7611e1b4bfd7d3aef
kaibad <badukailash001@gmail.com>
2023-01-26 20:41:09 +0545

    class-member-function

--- 3. CLASS AND OBJECTS/2class_member_function.cpp
+++ 3. CLASS AND OBJECTS/2class_member_function.cpp
@@ -1,7 +1,7 @@
 // using set method to initialize member-variables and get method to retrive the valus of member variables
 
 #include <iostream>
-#include <string>
+// #include <string>
 using namespace std;
 class Student
 {
@@ -12,7 +12,7 @@ public:
     double total_marks;
 
     // member functions
-    void setNmae(string n);
+    void setName(string n);
     void setAge(int a);
     void setMarks(double m);
     void display();
@@ -22,13 +22,57 @@ public:
     double getMarks();
 };
 
-void Student::setNmae(string n)
+void Student::setName(string n)
 {
     name = n;
 }
-
+void Student::setAge(int a)
+{
+    age = a;
+}
+void Student::setMarks(double m)
+{
+    total_marks = m;
+}
+string Student::getName()
+{
+    return name;
+}
+int Student::getAge()
+{
+    return age;
+}
+double Student::getMarks()
+{
+    return total_marks;
+}
+void Student::display()
+{
+    cout << "\n\nFollowing are student info.:-" << endl;
+    cout << "Name:-" << getName() << endl;
+    cout << "Name:-" << getAge() << endl;
+    cout << "Name:-" << getMarks() << endl;
+}
 int main()
 {
+    Student s;
+    s.setName("kailash badu");
+    s.setAge(20);
+    s.setMarks(99.99);
+    s.display();
 
-    return 0;
+    // return 0;
 }
+/*In the given code, there is no output because
+the return statement in the main function is returning
+control back to the operating system before the
+display function is called.
+The return statement in the main function is typically
+used to indicate the status of the program's execution to
+the operating system. In C++, the main function is expected
+to return an integer value, and a return value of 0 typically
+indicates that the program has executed successfully.
+You can remove the "return 0" statement from the main
+function, so that the program will continue to execute
+after the display function is called and you will be able
+to see the student information.*/
